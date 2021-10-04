f91eedf16f09c956b82eb40ca659c2c0a713b01a
Alexandre Pedrecal Campos <pedrecalc@gmail.com>
2021-10-04 09:26:17 -0300

    add error handling; add runtime type; finish chapter 3

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -18,6 +18,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and couldn't proccess your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	Title     string    `json:"title,omitempty"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
